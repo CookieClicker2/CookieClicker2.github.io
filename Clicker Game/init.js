cd397468a913dc40e351e4902d38d8e05257cdbd
yodaheYT <94496398+yodaheYT@users.noreply.github.com>
2022-02-07 17:56:32 -0700

    Delete init.js

--- Clicker Game/init.js
+++
@@ -1,9 +0,0 @@
-if (!localStorage.getItem('Cookies')) {
-    localStorage.setItem('Cookies', 100)
-}
-if (!localStorage.getItem('Multi')) {
-    localStorage.setItem('Multi', 1)
-}
-function click() {
-    localStorage.setItem('Cookies', localStorage.getItem('Cookies')+=(1*localStorage.getItem('Multi')))
-}
